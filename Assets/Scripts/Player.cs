a5c69165666a7db0fc02720ccef791b9eef6161c
emsky <mateusz.emsky@gmail.com>
2018-12-07 20:33:23 +0100

    Shooting in Coroutine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,21 +9,36 @@ public class Player : MonoBehaviour {
     [SerializeField] float padding = 1f;
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine fireCoroutine;
+
     float xMin;
     float xMax;
     float yMin;
     float yMax;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         SetUpMoveBoundaries();   // Boundaries - ang. granice
-        
-	}
+    }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         Move();
         Fire();
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
 
     private void Move()
@@ -49,8 +64,11 @@ public class Player : MonoBehaviour {
     {
         if(Input.GetButtonDown("Fire1"))
         {
-            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
-            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            fireCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireCoroutine);
         }
     }
 
